d6201ac7e6db499801c812f1c495ef178a6b8d06
Luthfy Chaerudy <luthfy.chaerudy218@gmail.com>
2022-04-21 22:38:16 +0700

    redeploy

--- vynilstore/src/views/MyCart.vue
+++ vynilstore/src/views/MyCart.vue
@@ -24,7 +24,7 @@
             </table>
         </div>
         <button v-if="!invoice" type="button" class="mt-7 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 " @click.prevent="cart">check out</button>
-        <a v-else type="button" class="mt-7 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 " :href="invoice" target="_blank">pay</a>
+        <a v-else type="button" class="mt-7 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 " :href="invoice">pay</a>
     </div>
 </template>
 
